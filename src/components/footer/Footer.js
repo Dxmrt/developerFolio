f9983f7e0162c2826a2d7de85a8d8f557151ec27
SVG <54545515+Dxmrt@users.noreply.github.com>
2023-05-10 00:05:13 +0200

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Douxxie.eth 2023")}
+          {emoji("Douxxie.eth 2023 ©")}
         </p>
       </div>
     </Fade>
